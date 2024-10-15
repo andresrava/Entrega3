2da686d76c0d5bc53c3146f7259e542d66e8e78d
Andrés Rava <andresravamail@gmail.com>
2024-10-14 23:28:59 -0300

    Crea la lista en la sessionstorage. Problema con el boton del primer elemento.

--- js/functions.js
+++ js/functions.js
@@ -4,3 +4,21 @@ function Articulo(id, nombre, precio, imagen) {
     this.precio = precio;
     this.imagen = imagen;
 }
+
+
+
+
+function AgregaAlCarrito(codigo) {
+    
+    let lista = sessionStorage.getItem('carrito');
+    if (!lista) {
+        lista = [];
+    } else {
+        lista = JSON.parse(lista);
+        
+    }
+    lista.push(codigo);
+    sessionStorage.setItem('carrito', JSON.stringify(lista));
+    console.log(lista);
+}
+
